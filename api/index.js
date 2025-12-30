77b0f669fbdff0204dd112f0a670a0e1c7bf2559
VeryFach <162655143+VeryFach@users.noreply.github.com>
2025-12-30 23:58:38 +0700

    Refactor server initialization and update Vercel configuration

--- api/index.js
+++ api/index.js
@@ -1,3 +1,19 @@
-const server = require('../dist/main').default;
+const { NestFactory } = require('@nestjs/core');
+const { AppModule } = require('../dist/app.module');
 
-export default server;
+let cachedServer;
+
+async function bootstrap() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+  return cachedServer;
+}
+
+module.exports = async (req, res) => {
+  const server = await bootstrap();
+  return server(req, res);
+};
